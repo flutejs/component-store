a265a4e874997037115e799fc0917994d23ebffd
zinkey <yaya@uloveit.com.cn>
2016-06-08 21:46:53 +0800

    0.2.0

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (defaultStore = {}, name = 'onChange') => ClassA => props => {
-  let nextStore = {};
-  const xprops = {
-    ...defaultStore,
-    ...props,
-    [name](obj) {
-      nextStore = {
-        ...nextStore,
-        ...obj,
-      };
-      props[name]({
-        ...defaultStore,
-        ...props,
-        ...nextStore,
-      });
-    },
-  };
-  return <ClassA {...xprops} />;
+export default (defaultStore = {}, name = 'onChange') => ClassA => class extends Component {
+  nextStore = {};
+  onChange = obj => {
+    this.nextStore = {
+      ...this.nextStore,
+      ...obj,
+    };
+    this.props[name]({
+      ...defaultStore,
+      ...this.props,
+      ...this.nextStore,
+    });
+  }
+  render() {
+    const diffStore = {};
+    Object.keys(this.nextStore).forEach(key => {
+      if (key in this.props) {
+        return;
+      }
+      diffStore[key] = this.nextStore[key];
+    });
+    const xprops = {
+      ...defaultStore,
+      ...this.props,
+      [name]: this.onChange,
+      ...diffStore,
+    };
+    return <ClassA {...xprops} />;
+  }
 };
